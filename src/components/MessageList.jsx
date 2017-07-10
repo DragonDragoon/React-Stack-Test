2deee75c3ddb143f25da7cb00b4b122453a667ba
William Woodard <william@woodard.us>
2017-07-09 23:37:17 -0400

    Installing and config firebase

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -2,23 +2,44 @@ import React from 'react';
 import Message from './Message.jsx';
 import Card from 'material-ui/card';
 import List from 'material-ui/list';
+import * as Firebase from 'firebase';
+import _ from 'lodash';
 
 class MessageList extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      messages: [
-        'hi, how are you?',
-        'i am fine, and you?'
-      ]
+      messages: []
     };
+
+
+    // Set the configuration for your app
+    // TODO: Replace with your project's config object
+    let config = {
+      apiKey: 'AIzaSyCV5CW4WFKRdrGrw9rUc8MvPmNglzkz0GY',
+      authDomain: 'react-stack-test-21832.firebaseapp.com',
+      databaseURL: 'https://react-stack-test-21832.firebaseio.com/',
+      storageBucket: 'react-stack-test-21832.appspot.com'
+    };
+
+    Firebase.initializeApp(config);
+
+    // Get a reference to the database service
+    let database = Firebase.database();
+
+    database.ref('/messages/').once('value').then((snapshot) => {
+      let messages = snapshot.val();
+      this.setState({
+        messages: messages
+      });
+    });
   }
 
   render() {
     var messageNodes = this.state.messages.map((message) => {
       return (
-        <Message message={message} />
+        <Message message={message.message} />
       );
     });
 
